c594ab3f5910e4cced7b3c04e88fbd489d32d222
valkoz <kozlov.valent@gmail.com>
2018-09-09 11:15:07 +0300

    Sorry

--- app/src/main/java/com/github/valkoz/chargemonitor/MainActivity.kt
+++ app/src/main/java/com/github/valkoz/chargemonitor/MainActivity.kt
@@ -11,7 +11,7 @@ import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    private val baseUrl = "https://6c1fe2e2.ngrok.io"
+    private val baseUrl = "https://bf4d3194.ngrok.io"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
